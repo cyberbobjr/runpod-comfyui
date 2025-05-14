22ec3454799a480f35f9e3fac7c4684e5ac4f9ad
cyberbobjr <cyberbobjr@yahoo.com>
2025-05-14 17:46:33 +0200

    log rotate

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Pour lancer en tâche de fond (background) et écrire les logs dans un fichier :
-nohup uvicorn main:app --reload --host 0.0.0.0 --port 8081 > server.log 2>&1 &
+# Limite la taille du fichier de log à 10 Mo, conserve 3 archives (logrotate simple via logrotate si dispo)
+LOGFILE="/var/log/comfyui-server.log"
+MAXSIZE=10485760 # 10 Mo
+
+if [ -f "$LOGFILE" ] && [ $(stat -c%s "$LOGFILE") -ge $MAXSIZE ]; then
+    mv "$LOGFILE" "$LOGFILE.1"
+    [ -f "$LOGFILE.1" ] && mv "$LOGFILE.1" "$LOGFILE.2"
+    [ -f "$LOGFILE.2" ] && mv "$LOGFILE.2" "$LOGFILE.3"
+fi
+
+nohup uvicorn main:app --reload --host 0.0.0.0 --port 8081 >> "$LOGFILE" 2>&1 &
